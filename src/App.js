931c9624d147ef5b66998ea7d0fd02df0cb797d6
Blake Hoff <bingbangbrine@gmail.com>
2025-06-17 12:49:04 -0700

    fixed url for github in package.json, and added link to calculate tax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://smartasset.com/taxes/income-taxes#pInHykDBif"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Find your income tax percentage
         </a>
       </header>
+
     </div>
   );
 }
